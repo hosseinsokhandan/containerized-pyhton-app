3b25567b912be4dc4d0abdf14b4b914186c891ac
hosseinsokhandan <iamsokhandan@gmail.com>
2021-10-23 17:18:15 +0330

    PROEJCT_NAME in dev.env

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
+
+STATIC_ROOT = BASE_DIR.parent / "cdn" / "static"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.parent / "cdn" / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
